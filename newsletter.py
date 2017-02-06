cb968f5cefe8c80971b87101a96abf439dd1d4e9
Severin Schols <severin@schols.de>
2017-02-06 01:20:49 +0100

    sending emails, text wrap, and python env reqs

--- newsletter.py
+++ newsletter.py
@@ -1,12 +1,19 @@
-
 from trello import TrelloClient
 from datetime import date
-from sys import exit
+from sys import exit, argv
+from email.message import Message
+import smtplib
+from textwrap import wrap
+
 import config
 
 def debug_out(str):
     if config.debug:
         print "-- %s" % str
+
+if len(argv) != 2 or argv[1] not in ["preview", "final"]:
+    print "Usage: newsletter.py [preview|final]"
+    exit(1)
 
 client = TrelloClient(
     api_key=config.api_key,
@@ -48,19 +55,49 @@ cards = lists[0].list_cards()
 
 debug_out("List found, with %s cards" % len(cards))
 
+if len(cards) == 0:
+    print "Empty list. Not sending anything"
+    exit(1)
+
 for card in cards:
     if card.description != "":
         out.append("* %s" % card.name)
-        out.append("%s" % card.description)
+        out += wrap(card.description)
         out.append("")
     else:
         debug_out("Card '%s' has no description text. Skipping" % card.name)
 
+subject = "MuMaNews - CW %02d" % week
+
+if argv[1] == "preview":
+    subject = "[PREVIEW] %s" % subject
+
+out = wrap(config.pre_bullet_points) + [""] + out + wrap(config.post_bullet_points)
+body = "\n".join(out)
+
 debug_out("Finished generating newsletter.")
 debug_out("Generated output:")
-print "Subject: MuMaNews - CW %02d" % week
+print "Subject: %s" % subject
 print
-print config.pre_bullet_points
-print
-print "\n".join(out)
-print config.post_bullet_points
+print body
+
+if argv[1] == "preview":
+    rcpt = config.MAIL_TO_PREVIEW
+elif argv[1] == "final":
+    rcpt = config.MAIL_TO
+
+msg = Message()
+msg.set_payload(body, "utf-8")
+msg["Subject"] = subject
+msg["From"] = config.MAIL_FROM
+msg["To"] = rcpt
+
+debug_out("Sending message")
+server = smtplib.SMTP(config.SMTP_HOST, config.SMTP_PORT)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(config.SMTP_USER, config.SMTP_PASS)
+text = msg.as_string()
+server.sendmail(config.MAIL_FROM, rcpt.split(","), text)
+server.quit()
